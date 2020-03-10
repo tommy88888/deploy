b473e0fc9244879a7b0f986c5474c944ce499861
tommy <tommydewa08@gmail.com>
2020-03-10 10:43:04 +0700

    Updates

---
+++ precache-manifest.d7f4ed95da924a8df2446d99fe3ebeb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72162db7ebf14377ca67d3fd54b7c16d",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "7bdf1cd5dc82d5c7fc89",
+    "url": "/deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28957307754b640bd779",
+    "url": "/deploy/static/js/2.077def08.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/deploy/static/js/2.077def08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bdf1cd5dc82d5c7fc89",
+    "url": "/deploy/static/js/main.175f9fa9.chunk.js"
+  },
+  {
+    "revision": "f56ef5611e9ba541f6ad",
+    "url": "/deploy/static/js/runtime-main.797f2590.js"
+  }
+]);
